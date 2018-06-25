c033e1305b7468409c2139b2a14f435ac0697833
Gary Feng <gary.feng@osellus-mobile.com>
2018-06-25 16:34:38 -0400

    Add button to go to RREFActivity

--- app/src/main/java/projects/feng/gary/matrixtools/MainActivity.kt
+++ app/src/main/java/projects/feng/gary/matrixtools/MainActivity.kt
@@ -1,9 +1,16 @@
 package projects.feng.gary.matrixtools
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import projects.feng.gary.matrixtools.R.id.*
+
+const val EXTRA_NUM_ROWS = "number_of_rows"
+const val EXTRA_NUM_COLS = "number_of_cols"
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.rrefButton).setOnClickListener {
+        /*findViewById<View>(R.id.rrefButton).setOnClickListener {
             val text = findViewById<TextView>(R.id.text)
 
             val test = arrayOf(Fraction(5), Fraction(3), Fraction(2),
@@ -28,7 +35,24 @@ class MainActivity : AppCompatActivity() {
                 }
                 text.append("\n")
             }
-        }
+        }*/
+
+        val rrefButton = rrefCalculatorButton as Button
+        rrefButton.setOnClickListener({
+            val numRows = (numRowsEditText as EditText).getIntText()
+            val numCols = (numColsEditText as EditText).getIntText()
+
+            val intent = Intent(this, RREFActivity::class.java).apply {
+                putExtra(EXTRA_NUM_ROWS, numRows)
+                putExtra(EXTRA_NUM_COLS, numCols)
+            }
+
+            startActivity(intent)
+        })
+    }
+
+    fun EditText.getIntText(): Int {
+        return text.toString().toInt()
     }
 
 
